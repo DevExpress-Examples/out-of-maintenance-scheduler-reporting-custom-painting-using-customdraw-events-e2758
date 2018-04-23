b6de3e52e09870b72861db825b7907c157a05a2d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/SchedulerReportCustomDraw/Form1.Designer.vb
@@ -0,0 +1,113 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace SchedulerReportCustomDraw
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+			Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+			Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+			Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+			Me.btnShowReport = New DevExpress.XtraEditors.SimpleButton()
+			Me.dateNavigator1 = New DevExpress.XtraScheduler.DateNavigator()
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.panelControl1.SuspendLayout()
+			CType(Me.dateNavigator1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' schedulerControl1
+			' 
+			Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.schedulerControl1.Location = New System.Drawing.Point(0, 43)
+			Me.schedulerControl1.Name = "schedulerControl1"
+			Me.schedulerControl1.Size = New System.Drawing.Size(696, 503)
+			Me.schedulerControl1.Start = New System.DateTime(2010, 12, 13, 0, 0, 0, 0)
+			Me.schedulerControl1.Storage = Me.schedulerStorage1
+			Me.schedulerControl1.TabIndex = 0
+			Me.schedulerControl1.Text = "schedulerControl1"
+			Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+			Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+			' 
+			' panelControl1
+			' 
+			Me.panelControl1.Controls.Add(Me.btnShowReport)
+			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
+			Me.panelControl1.Location = New System.Drawing.Point(0, 0)
+			Me.panelControl1.Name = "panelControl1"
+			Me.panelControl1.Size = New System.Drawing.Size(875, 43)
+			Me.panelControl1.TabIndex = 1
+			' 
+			' btnShowReport
+			' 
+			Me.btnShowReport.Location = New System.Drawing.Point(12, 12)
+			Me.btnShowReport.Name = "btnShowReport"
+			Me.btnShowReport.Size = New System.Drawing.Size(75, 23)
+			Me.btnShowReport.TabIndex = 0
+			Me.btnShowReport.Text = "Show Report"
+'			Me.btnShowReport.Click += New System.EventHandler(Me.btnShowReport_Click);
+			' 
+			' dateNavigator1
+			' 
+			Me.dateNavigator1.Dock = System.Windows.Forms.DockStyle.Right
+			Me.dateNavigator1.HotDate = Nothing
+			Me.dateNavigator1.Location = New System.Drawing.Point(696, 43)
+			Me.dateNavigator1.Name = "dateNavigator1"
+			Me.dateNavigator1.SchedulerControl = Me.schedulerControl1
+			Me.dateNavigator1.Size = New System.Drawing.Size(179, 503)
+			Me.dateNavigator1.TabIndex = 1
+'			Me.dateNavigator1.CustomDrawDayNumberCell += New DevExpress.XtraEditors.Calendar.CustomDrawDayNumberCellEventHandler(Me.dateNavigator1_CustomDrawDayNumberCell);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(875, 546)
+			Me.Controls.Add(Me.schedulerControl1)
+			Me.Controls.Add(Me.dateNavigator1)
+			Me.Controls.Add(Me.panelControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.panelControl1.ResumeLayout(False)
+			CType(Me.dateNavigator1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+		Private schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+		Private panelControl1 As DevExpress.XtraEditors.PanelControl
+		Private WithEvents btnShowReport As DevExpress.XtraEditors.SimpleButton
+		Private WithEvents dateNavigator1 As DevExpress.XtraScheduler.DateNavigator
+	End Class
+End Namespace
+
---
+++ VB/SchedulerReportCustomDraw/XtraSchedulerReport1.Designer.vb
@@ -0,0 +1,121 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace SchedulerReportCustomDraw
+	Partial Public Class XtraSchedulerReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.calendarControl1 = New DevExpress.XtraScheduler.Reporting.CalendarControl()
+			Me.dayViewTimeCells1 = New DevExpress.XtraScheduler.Reporting.DayViewTimeCells()
+			Me.horizontalDateHeaders1 = New DevExpress.XtraScheduler.Reporting.HorizontalDateHeaders()
+			Me.reportDayView1 = New DevExpress.XtraScheduler.Reporting.ReportDayView()
+			Me.schedulerStoragePrintAdapter1 = New DevExpress.XtraScheduler.Reporting.SchedulerStoragePrintAdapter()
+			Me.horizontalWeek1 = New DevExpress.XtraScheduler.Reporting.HorizontalWeek()
+			Me.horizontalResourceHeaders1 = New DevExpress.XtraScheduler.Reporting.HorizontalResourceHeaders()
+			CType(Me.reportDayView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.schedulerStoragePrintAdapter1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.horizontalResourceHeaders1, Me.calendarControl1, Me.horizontalDateHeaders1, Me.dayViewTimeCells1})
+			Me.Detail.HeightF = 581.25F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.PageBreak = DevExpress.XtraReports.UI.PageBreak.AfterBand
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' calendarControl1
+			' 
+			Me.calendarControl1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.calendarControl1.Name = "calendarControl1"
+			Me.calendarControl1.SizeF = New System.Drawing.SizeF(650F, 147.5834F)
+			Me.calendarControl1.TimeCells = Me.dayViewTimeCells1
+			Me.calendarControl1.View = Me.reportDayView1
+			' 
+			' dayViewTimeCells1
+			' 
+			Me.dayViewTimeCells1.HorizontalHeaders = Me.horizontalDateHeaders1
+			Me.dayViewTimeCells1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 198.5834F)
+			Me.dayViewTimeCells1.Name = "dayViewTimeCells1"
+			Me.dayViewTimeCells1.ShowWorkTimeOnly = True
+			Me.dayViewTimeCells1.SizeF = New System.Drawing.SizeF(650F, 374.75F)
+			Me.dayViewTimeCells1.View = Me.reportDayView1
+			Me.dayViewTimeCells1.VisibleTimeSnapMode = False
+			' 
+			' horizontalDateHeaders1
+			' 
+			Me.horizontalDateHeaders1.HorizontalHeaders = Me.horizontalResourceHeaders1
+			Me.horizontalDateHeaders1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 172.5834F)
+			Me.horizontalDateHeaders1.Name = "horizontalDateHeaders1"
+			Me.horizontalDateHeaders1.SizeF = New System.Drawing.SizeF(650F, 25.99998F)
+			Me.horizontalDateHeaders1.View = Me.reportDayView1
+			' 
+			' reportDayView1
+			' 
+			Me.reportDayView1.GroupType = DevExpress.XtraScheduler.SchedulerGroupType.Date
+			Me.reportDayView1.VisibleDayCount = 3
+			' 
+			' schedulerStoragePrintAdapter1
+			' 
+			Me.schedulerStoragePrintAdapter1.TimeInterval.Duration = System.TimeSpan.Parse("123.00:00:00")
+			Me.schedulerStoragePrintAdapter1.TimeInterval.Start = New System.DateTime(2010, 12, 12, 0, 0, 0, 0)
+			' 
+			' horizontalWeek1
+			' 
+			Me.horizontalWeek1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 0F)
+			Me.horizontalWeek1.Name = "horizontalWeek1"
+			Me.horizontalWeek1.SizeF = New System.Drawing.SizeF(550F, 150F)
+			' 
+			' horizontalResourceHeaders1
+			' 
+			Me.horizontalResourceHeaders1.LocationFloat = New DevExpress.Utils.PointFloat(0F, 147.5834F)
+			Me.horizontalResourceHeaders1.Name = "horizontalResourceHeaders1"
+			Me.horizontalResourceHeaders1.SizeF = New System.Drawing.SizeF(650F, 25F)
+			Me.horizontalResourceHeaders1.View = Me.reportDayView1
+			' 
+			' XtraSchedulerReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail})
+			Me.SchedulerAdapter = Me.schedulerStoragePrintAdapter1
+			Me.Version = "13.1"
+			Me.Views.AddRange(New DevExpress.XtraScheduler.Reporting.ReportViewBase() { Me.reportDayView1})
+			CType(Me.reportDayView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.schedulerStoragePrintAdapter1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private schedulerStoragePrintAdapter1 As DevExpress.XtraScheduler.Reporting.SchedulerStoragePrintAdapter
+		Private horizontalWeek1 As DevExpress.XtraScheduler.Reporting.HorizontalWeek
+		Private dayViewTimeCells1 As DevExpress.XtraScheduler.Reporting.DayViewTimeCells
+		Private reportDayView1 As DevExpress.XtraScheduler.Reporting.ReportDayView
+		Private calendarControl1 As DevExpress.XtraScheduler.Reporting.CalendarControl
+		Private horizontalDateHeaders1 As DevExpress.XtraScheduler.Reporting.HorizontalDateHeaders
+		Private horizontalResourceHeaders1 As DevExpress.XtraScheduler.Reporting.HorizontalResourceHeaders
+	End Class
+End Namespace
